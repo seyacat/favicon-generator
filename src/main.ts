7c032d39e0e893599155c2f5f989811f3496e2cd
seyacat <seyacat@gmail.com>
2026-05-28 12:07:19 -0500

    chore: migrar deps a @closerclick/*

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import '@gatoseya/closer-click-support'
+import '@closerclick/closer-click-support'
 
 createApp(App).mount('#app')
